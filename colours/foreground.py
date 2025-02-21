e8ff8260ba99818121d8c6dceea513dee281d7de
Sapphicat247 <jacobzassbangham@gmail.com>
2025-02-21 16:15:40 +0000

    Add more colours

--- colours/foreground.py
+++ colours/foreground.py
@@ -1,9 +1,14 @@
+BLACK = "\033[30m"
+GREY = "\033[90m"
+WHITE = "\033[97m"
+
 RED = "\033[91m"
 GREEN = "\033[92m"
 YELLOW = "\033[93m"
 BLUE = "\033[94m"
 MAGENTA = "\033[95m"
 CYAN = "\033[96m"
+
 
 def RGB(r: int, g: int, b: int):
     # sets foreground colour to RGB values
